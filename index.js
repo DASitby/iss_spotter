e94318f04ca415911d4b0b2559bb17f3484ee9d0
DASitby <davidsooley551@gmail.com>
2022-12-16 23:39:41 +0000

    add fetchCoordsbyIP

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const { fetchMyIP,fetchCoordsByIP } = require('./iss');
 
-fetchMyIP((error, ip) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
-  }
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
 
-  console.log('It worked! Returned IP:' , ip);
-});
+//   console.log('It worked! Returned IP:' , ip);
+// });
+
+// fetchCoordsByIP((error, coords) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned coords:' , coords);
+// });
--- iss.js
+++ iss.js
@@ -3,14 +3,32 @@ const request = require('request');
 const fetchMyIP = (cb) => {
   request('https://api.ipify.org?format=json',(error, response, body) => {
     if (error) {
-      return (error, null);
+      return cb(error, null);
     }
     if (response.statusCode !== 200) {
       const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
-      return (Error(msg), null);
+      return cb(Error(msg), null);
     }
     const ip = JSON.parse(body).ip;
-    cb(null,ip);
+    return cb(null,ip);
   });
 };
-module.exports = {fetchMyIP,};
+const IP = fetchMyIP((error,ip) => ip);
+
+const fetchCoordsByIP = (cb) => {
+  request(`https://ipwho.is/`, (error, response, body) => {
+    if (error) {
+      return cb(error, null);
+    }
+    const parsedBody = JSON.parse(body);
+    if (!parsedBody.success) {
+      const message = `Success status was ${parsedBody.success}. Server message says: ${parsedBody.message} when fetching for IP ${parsedBody.ip}`;
+      cb(Error(message), null);
+      return;
+    }
+    const coords = {latitude: parsedBody.latitude, longitude: parsedBody.longitude};
+    cb(null, coords);
+  });
+};
+
+module.exports = {fetchMyIP, fetchCoordsByIP};
